be16a8c7749998062dee473f61febc46ea540df9
Jason Rigden <jasonrigden@gmail.com>
2019-02-13 08:47:07 -0800

    minor fixes

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DATA = {}
-DATA['BASE_URL'] = "http://127.0.0.1:35729"
-DATA['OUTPUT_DIR'] = "/home/jason/code/seattle-podcast-list/output/"
+DATA['BASE_URL'] = "https://seattlepodcasters.com/list"
+DATA['OUTPUT_DIR'] = "/home/jason/trypticon/Seattle_Podcast_List/"
 DATA['RSS_LIST_PATH'] = "/home/jason/code/seattle-podcast-list/rss_urls.txt"
